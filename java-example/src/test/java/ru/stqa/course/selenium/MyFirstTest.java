40dfb2eb95f4168a30527eb2085f7415f06f9ab8
Maria.Guseva <Maria.Guseva@billing.ru>
2016-11-16 13:02:10 +0300

    first test

---
+++ java-example/src/test/java/ru/stqa/course/selenium/MyFirstTest.java
@@ -0,0 +1,37 @@
+package ru.stqa.course.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+/**
+ * Created by Maria.Guseva on 15.11.2016.
+ */
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("http://www.google.com/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
