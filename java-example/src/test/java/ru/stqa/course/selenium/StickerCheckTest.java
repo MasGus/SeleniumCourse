175160829d2e0f77b7750c7cb7426d54dd5d7d6b
Maria.Guseva <Maria.Guseva@billing.ru>
2016-11-25 14:58:57 +0300

    StickerCheckTest added

---
+++ java-example/src/test/java/ru/stqa/course/selenium/StickerCheckTest.java
@@ -0,0 +1,24 @@
+package ru.stqa.course.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+/**
+ * Created by Maria.Guseva on 25.11.2016.
+ */
+public class StickerCheckTest extends TestBase{
+
+    @Test
+    public void test(){
+        driver.get("http://localhost/litecart");
+
+        List<WebElement> webElementList = driver.findElements(By.cssSelector("li.column.product"));
+        for (WebElement webElement : webElementList) {
+            Assert.assertTrue(webElement.findElements(By.cssSelector("div.sticker") ).size() == 1);
+        }
+    }
+}
