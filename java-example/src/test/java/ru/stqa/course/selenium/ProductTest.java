6593c3f2c92aa10c91941d9f10a29e6a60738f67
Maria.Guseva <Maria.Guseva@billing.ru>
2016-11-30 18:12:03 +0300

    ProductTest added

---
+++ java-example/src/test/java/ru/stqa/course/selenium/ProductTest.java
@@ -0,0 +1,34 @@
+package ru.stqa.course.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+/**
+ * Created by Maria.Guseva on 30.11.2016.
+ */
+public class ProductTest extends TestBase{
+    //В тестах проверяется только структурная правильность страницы. Соответственно, не нужно проверять:
+    //"стили цены на главной странице и на странице товара -- первая цена серая, зачёркнутая, маленькая, вторая цена красная жирная, крупная"
+
+    @Test
+    public void test(){
+        driver.get("http://localhost/litecart");
+
+        WebElement product = driver.findElement(By.cssSelector("#box-campaigns li"));
+        String regular_price = product.findElement(By.cssSelector(".regular-price")).getText();
+        String campaign_price = product.findElement(By.cssSelector(".campaign-price")).getText();
+        String name = product.findElement(By.cssSelector(".name")).getText();
+
+        product.click();
+        String r_price_in = driver.findElement(By.cssSelector(".regular-price")).getText();
+        String c_price_in = driver.findElement(By.cssSelector(".campaign-price")).getText();
+        String name_in = driver.findElement(By.cssSelector("h1.title")).getText();
+
+        Assert.assertEquals(regular_price, r_price_in);
+        Assert.assertEquals(campaign_price, c_price_in);
+        Assert.assertEquals(name, name_in);
+    }
+}
